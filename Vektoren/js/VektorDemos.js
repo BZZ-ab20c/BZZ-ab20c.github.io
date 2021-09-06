47cabe614fb041b41feeae781b79e6f352ae4e0a
Tim Irmler <zwazel@hotmail.com>
2021-09-06 13:47:17 +0200

    work in progress demos
    mouse position is a bit off

--- Vektoren/js/VektorDemos.js
+++ Vektoren/js/VektorDemos.js
@@ -9,16 +9,48 @@ function simulateWithId(canvasId) {
 function simulate(canvas, context) {
     let divParent = canvas.parentElement;
     let width = canvas.width = divParent.clientWidth,
-        height = canvas.height = 3 * width / 4;
+        height = canvas.height = 3 * width / 4,
+        springPoint = vector.create(width / 2, height / 2),
+        weight = particle.create(Math.random() * width, Math.random() * height,
+            50, Math.random() * Math.PI * 2, 0.1);
+
+    weight.radius = 20;
+    weight.friction = 0.9;
+
+    canvas.addEventListener("mousemove", function (event) {
+        let mousePos = getMousePosInCanvas(canvas, event);
+
+        springPoint.setX(event.x);
+        springPoint.setY(mousePos.y);
+    });
 
     update();
 
     function update() {
         context.clearRect(0, 0, width, height);
 
+        let distance = springPoint.subtract(weight.position);
+        distance.setLength(distance.getLength() - 100);
+        let springForce = distance.multiply(0.1);
+
+        weight.velocity.addTo(springForce);
+
+        weight.update();
+
         context.beginPath();
-        context.arc(width / 2, height / 2, 20, 0, Math.PI * 2, false);
+        context.arc(weight.position.getX(), weight.position.getY(), weight.radius, 0, Math.PI * 2, false);
         context.fill();
+
+        context.beginPath();
+        context.arc(springPoint.getX(), springPoint.getY(), 4, 0, Math.PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.moveTo(weight.position.getX(), weight.position.getY());
+        context.lineTo(springPoint.getX(), springPoint.getY());
+        context.stroke();
+
+        weight.doBounce(width, height, 1);
 
         requestAnimationFrame(update);
     }
