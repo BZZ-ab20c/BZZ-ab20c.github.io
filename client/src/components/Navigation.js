b1e3e8363e8d6c6686982e7283c358a2b14fec75
Tim Irmler <zwazel@hotmail.com>
2021-12-14 08:07:47 +0100

    yes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,13 +2,13 @@ import {Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <nav>
+        <div>
             <div className="navBar">Ich bin Navigation</div>
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/Vektoren'>Vektoren</Link></li>
             </ul>
-        </nav>
+        </div>
     );
 }
 
