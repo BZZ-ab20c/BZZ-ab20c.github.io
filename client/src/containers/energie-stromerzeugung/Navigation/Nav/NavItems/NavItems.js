1c6038287402ceba869eab2414628fb0062e72c4
Tim Irmler <zwazel@hotmail.com>
2021-12-18 21:57:38 +0100

    oke looks better

--- client/src/containers/energie-stromerzeugung/Navigation/Nav/NavItems/NavItems.js
+++ client/src/containers/energie-stromerzeugung/Navigation/Nav/NavItems/NavItems.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import './NavItems.css';
-import NavItem from 'NavItem/NavItem';
+import NavItem from "./NavItem/NavItem";
 
 const navigationItems = () => (
     <ul className="">
         <NavItem link="/" exact>Home</NavItem>
-        <NavItem link="/orders">Generator</NavItem>
-        <NavItem link="/orders">Wasserkraftwerk</NavItem>
-        <NavItem link="/orders">Solar</NavItem>
-        <NavItem link="/orders">Kraftwerk</NavItem>
+        <NavItem link="/generator">Generator</NavItem>
+        <NavItem link="/wasserkraft">Wasserkraftwerk</NavItem>
+        <NavItem link="/solar">Solar</NavItem>
+        <NavItem link="/kraftwerk">Kraftwerk</NavItem>
     </ul>
 );
 
--- client/src/containers/energie-stromerzeugung/Navigation/Nav/SideDrawer/SideDrawer.js
+++ client/src/containers/energie-stromerzeugung/Navigation/Nav/SideDrawer/SideDrawer.js
@@ -5,22 +5,22 @@ import classes from './SideDrawer.css';
 // import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-   let attachedClasses = [classes.SideDrawer, classes.Close];
-   if(props.open){
-      attachedClasses = [classes.SideDrawer, classes.Open];
-   }
-   return (
-      <div>
-         {/*<Backdrop show={props.open} clicked={props.closed}/>*/}
-         <div className={attachedClasses.join(' ')}>
-            <div className={classes.Logo}>
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <div>
+            {/*<Backdrop show={props.open} clicked={props.closed}/>*/}
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-               <nav>
-                  <NavigationItems />
-               </nav>
-         </div>
-      </div>
-   );
+        </div>
+    );
 }
 
 export default sideDrawer;
