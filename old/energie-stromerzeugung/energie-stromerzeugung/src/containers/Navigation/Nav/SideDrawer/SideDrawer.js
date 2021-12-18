4ffbeac31febf573fcfdb4b6ef4a5889e7cddd72
Tim Irmler <zwazel@hotmail.com>
2021-12-18 22:00:16 +0100

    readded them lol

---
+++ old/energie-stromerzeugung/energie-stromerzeugung/src/containers/Navigation/Nav/SideDrawer/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = (props) => {
+   let attachedClasses = [classes.SideDrawer, classes.Close];
+   if(props.open){
+      attachedClasses = [classes.SideDrawer, classes.Open];
+   }
+   return (
+      <div>
+         <Backdrop show={props.open} clicked={props.closed}/>
+         <div className={attachedClasses.join(' ')}>
+            <div className={classes.Logo}>
+            </div>
+               <nav>
+                  <NavigationItems />
+               </nav>
+         </div>
+      </div>
+   );
+}
+
+export default sideDrawer;
