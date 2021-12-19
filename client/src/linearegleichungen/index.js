11c65b4f0e0fa92ddeb6bc04ad8792960eb37218
shirin197 <nadina@wolke7.net>
2021-12-19 17:18:55 +0100

    huere scheiss

---
+++ client/src/linearegleichungen/index.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+const Linearegleichungen = () => {
+    return (
+        <Layout className="App">
+            <Navigation/>
+            <TitleSeite/>
+        </Layout>
+    );
+}
+
+export default Linearegleichungen;
