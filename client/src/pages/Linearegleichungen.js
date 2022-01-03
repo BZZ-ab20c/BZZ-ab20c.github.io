57b58bbfc27d05c6926b29c365e30d5146f38267
Tim Irmler <zwazel@hotmail.com>
2022-01-03 11:22:20 +0100

    fixed nadinas page... mostly

--- client/src/pages/Linearegleichungen.js
+++ client/src/pages/Linearegleichungen.js
@@ -1,30 +1,32 @@
 import Navigation from "../components/Navigation";
+import MathImg from "../img/lineargleichungen/math-img.jpg";
+import Wave01 from "../img/lineargleichungen/wave-01.png";
 
 const Linearegleichungen = () => {
     return (
         <div className="container">
             <Navigation/>
             <section id="banner">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
                             <h1>Lineare Gleichungssysteme</h1>
                         </div>
-                        <div class="w-100"></div>
-                        <div class="col">
+                        <div className="w-100"/>
+                        <div className="col">
                             Willkommen beim
-                            <span class="tag-01"><a id="link-01" href="https://www.bzz.ch/"
+                            <span className="tag-01"><a id="link-01" href="https://www.bzz.ch/"
                                                     target="_blank">BZZ</a></span>
                         </div>
                     </div>
                 </div>
             </section>
-            <div class="wave-01">
-                <img class="wave-img" src="img/wave-01.png"></img>
+            <div className="wave-01">
+                <img className="wave-img" src={Wave01}/>
             </div>
             <section id="second-banner">
-                <div class="container">
-                    <div class="col-01">
+                <div className="container">
+                    <div className="col-01">
                         <p>
                             <h2>Inhalt</h2>
                         </p>
@@ -32,9 +34,9 @@ const Linearegleichungen = () => {
                 </div>
             </section>
             <section id="section-txt">
-                <div class="container">
-                    <div class="row">
-                        <div class="sb-div-left">
+                <div className="container">
+                    <div className="row">
+                        <div className="sb-div-left">
                             <p>
                                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
                                 invidunt ut
@@ -56,23 +58,23 @@ const Linearegleichungen = () => {
                                 sanctus est Lorem ipsum dolor sit amet.
                             </p>
                         </div>
-                        <div class="sb-div-right-img">
-                            <img class="math-img" src="img/math-img.jpg"></img>
+                        <div className="sb-div-right-img">
+                            <img className="math-img" src={MathImg}/>
                         </div>
                     </div>
                 </div>
             </section>
             <section id="section-btn-title">
-                <div class="container">
-                    <div class="row">
-                        <div class="tag-02">
+                <div className="container">
+                    <div className="row">
+                        <div className="tag-02">
                             <p>
                                 Systeme
                             </p>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="tag-03">
+                    <div className="row">
+                        <div className="tag-03">
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
                             invidunt ut
                             labore et
@@ -83,22 +85,22 @@ const Linearegleichungen = () => {
                 </div>
             </section>
             <section id="section-btn">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <div class="btn-01">
-                                <a class="link" href="am.html">
-                                    <div class="btn-01-1">
-                                        <p class="btn-01-1-txt">Additionsmethode</p>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="btn-01">
+                                <a className="link" href="am.html">
+                                    <div className="btn-01-1">
+                                        <p className="btn-01-1-txt">Additionsmethode</p>
                                     </div>
                                 </a>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="btn-02">
-                                <a class="link" href="subm.html">
-                                    <div class="btn-02-2">
-                                        <p class="btn-02-2-txt">Subtitutionsmethode</p>
+                        <div className="col">
+                            <div className="btn-02">
+                                <a className="link" href="subm.html">
+                                    <div className="btn-02-2">
+                                        <p className="btn-02-2-txt">Subtitutionsmethode</p>
                                     </div>
                                 </a>
                             </div>
