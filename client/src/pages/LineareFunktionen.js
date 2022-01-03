cf292bed32d3939a500a25dbbd612ab4c14d7474
Tim Irmler <zwazel@hotmail.com>
2022-01-03 13:42:42 +0100

    lineare funktionen

--- client/src/pages/LineareFunktionen.js
+++ client/src/pages/LineareFunktionen.js
@@ -4,7 +4,19 @@ const LineareFunktionen = () => {
     return (
         <div>
             <Navigation/>
-            <div className={"container"}>LineareFunktionen</div>
+            <div className={"container"}>
+                <h1>Theorie Lineare-Funktionen / Geradengleichungen</h1>
+                <p>Legende:
+                    <br/>
+                    Lineare-Funktionen = Geradengleichungen
+                    <br/>
+                    m = Steigung
+                    <br/>
+                    b = y-Achsenabschnitt
+                </p>
+            </div>
+
+
         </div>
     );
 }
