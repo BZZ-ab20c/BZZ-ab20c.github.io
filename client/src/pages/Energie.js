9acb2b6cad6bb7eb81100b3cf597eee6ab891590
Tim Irmler <zwazel@hotmail.com>
2022-01-03 11:43:38 +0100

    energie mit navigation

--- client/src/pages/Energie.js
+++ client/src/pages/Energie.js
@@ -1,8 +1,13 @@
+import Navigation from "../components/Navigation";
+
 const Energie = () => {
     return (
-        <div className="container d-flex flex-column align-items-center justify-content-center">
-            <h1>Energie</h1>
-            <br/>
+        <div>
+            <Navigation/>
+            <div className="container d-flex flex-column align-items-center justify-content-center">
+                <h1>Energie</h1>
+                <br/>
+            </div>
         </div>
     );
 }
