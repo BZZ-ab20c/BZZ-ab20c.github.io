1e2a5607fa72c3a3727aadcece5ee343860b0aa2
Tim Irmler <zwazel@hotmail.com>
2022-01-03 11:36:29 +0100

    sollte jetzt komplett sein

--- client/src/pages/lineareGleichungenSubPages/subm.js
+++ client/src/pages/lineareGleichungenSubPages/subm.js
@@ -1,6 +1,28 @@
+import Wave01 from "../../img/lineargleichungen/wave-01.png";
+
 const SUBM = () => {
     return (
-        <div className={"container"}>SUBM</div>
+        <div className={"container"}>
+            {/*Banner*/}
+            <section id="banner">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1>Substitutionsverfahren</h1>
+                        </div>
+                        <div className="w-100"/>
+                        <div className="col">
+                            Lernset vom
+                            <span className="tag-01"><a id="link-01" href="https://www.bzz.ch/" target="_blank">BZZ</a></span>
+                            {/*Todo: Kennzeichnung das BZZ ein Link ist*/}
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className="wave-01">
+                <img className="wave-img" src={Wave01} alt={"wave img idk"}/>
+            </div>
+        </div>
     );
 }
 
