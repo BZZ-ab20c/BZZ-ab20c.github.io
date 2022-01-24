b3706b36c22dfde1bda241dbccbe032e90c7653a
Loic190 <flueckigerl@bzz.ch>
2022-01-24 12:35:14 +0100

    ...

--- client/src/pages/LineareFunktionen.js
+++ client/src/pages/LineareFunktionen.js
@@ -3,6 +3,12 @@ import theorie from "../img/linearefunktionen/theorie.jpg";
 import geradensteigung from "../img/linearefunktionen/geradensteigung.jpg";
 import steigunggleichung from "../img/linearefunktionen/steigunggleichung.jpg";
 import aufgabe_d from "../img/linearefunktionen/aufgabe_d.jpg";
+import yabschnitt from "../img/linearefunktionen/yabschnitt.jpg";
+import y_beispiel from "../img/linearefunktionen/y_beispiel.jpg";
+import y_beispiel2 from "../img/linearefunktionen/y_beispiel2.jpg";
+import y_beispiel3 from "../img/linearefunktionen/y_beispiel3.jpg";
+import y_beispiel4 from "../img/linearefunktionen/y_beispiel4.jpg";
+import y_beispiel5 from "../img/linearefunktionen/y_beispiel5.jpg";
 import React from "react";
 
 function togglelosungen() {
@@ -75,13 +81,66 @@ const LineareFunktionen = () => {
                 <p>
                     <h3>Der y-Achsenabschnitt</h3><br/>
                     <p>
-                        <img id="myImage" src={} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                        <img id="myImage" src={y_abscnitt} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
                     </p>
                     Der y-Achsenabschnitt beträgt 1,5. Der dazugehörige x-Wert ist 0.
 
                     Die Funktion schneidet die y-Achse an dem Punkt, wo der x-Wert null ist. Vorsicht! Die beiden Achsen<br/>
                     dürfen nicht verwechselt werden: Die x-Achse verläuft von links nach rechts und die y-Achse von unten nach oben.<br/>
                     Die Funktionsgleichung mit hilfe des y-Achsenabschnitt bestimmen.<br/>
+
+                    Bestimmen der Funktionsgleichung mit dem y-Achsenabschnitt<br/>
+
+                    <p>
+                        <img id="myImage" src={y_beispiel} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                    </p>
+                    Dieser Funktion können wir sofort ansehen, dass der y-Achsenabschnitt 4 beträgt,.<br/>
+                    da die Funktion die y-Achse an dieser Stelle schneidet.<br/>
+                    Auch die Steigung können wir durch bloßes Hinschauen herausfinden. Wenn wir eine Einheit x nach rechts gehen,.<br/>
+                    müssen wir eine Einheit in y-Richtung nach oben gehen.<br/>
+                    <p>
+                        <img id="myImage" src={y_beispiel2} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                    </p>
+                    Damit beträgt die Steigung 1.<br/><br/>
+
+                    Wir müssen die vollständige Funktionsgleichung bestimmen. Dafür setzen wir die beiden ermittelten Werte in die Gleichung ein.<br/>
+                    m = 1<br/>
+                    n = 4<br/>
+                    f(x) = m ⋅ x + n<br/>
+                    f(x) = 1 ⋅ x + 4<br/><br/>
+
+                    y-Achsenabschnitt mit der Steigung bestimmen<br/>
+                    Es kann sein, dass eine Abbildung eines Graphen gegeben ist, bei dem der Schnittpunkt mit der y-Achse nicht sichtbar ist.<br/>
+                    Wie beispielsweise bei dieser Abbildung:<br/>
+
+                    <p>
+                        <img id="myImage" src={y_beispiel3} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                    </p>
+                    Dieser Graph ist gegeben und der y-Achsenabschnitt soll ermittelt werden. Um die komplette Gleichung bestimmen zu können,<br/>
+                    fehlt der y-Achsenabschnitt. Dieser ist in unserem Graph nicht eingezeichnet, kann also nicht einfach abgelesen werden.<br/><br/>
+
+                    Um den Ordinatenabschnitt berechnen zu können, nutzen wir die Formel zur Berechnung der Steigung. Die Steigung errechnen wir als erstes.<br/>
+                    Sie beträgt m = 1,5. Doch mit dieser Formel können wir jetzt auch den y-Achsenabschnitt ermitteln. Hierfür setzen wir die Koordinaten<br/>
+                    eines uns bekannten Punktes und die Steigung der Funktion in die folgende Formel ein:<br/>
+                    <p>
+                        <img id="myImage" src={y_beispiel4} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                    </p>
+                    Wir setzen die Steigung m=1,5 und die Koordinaten des Punktes A(2|1) ein. Die rote 0 ist die x-Koordinate<br/>
+                    des y-Achsenabschnittes. Alles in die Formel eingesetzt ergibt sich:<br/>
+                    <p>
+                        <img id="myImage" src={y_beispiel5} style={{height: "100px", marginRight: 0}} alt={""}/><br/>
+                    </p>
+                    Jetzt müssen wir die Formel nur noch nach y2 umstellen und wir haben den y-Achsenabschnitt.<br/>
+                    Der erste Schritt ist also die Multiplikation mit −2. Es folgt:<br/>
+                    1,5⋅(−2)=y2−1<br/>
+                    Im nächsten Schritt addieren wir 1, um den Wert y2 allein auf einer Seite zu haben. Es ergibt sich:<br/>
+
+                    −3+1=y2→y2=−2
+                    Der y-Achsenabschnitt der Funktion beträgt also −2.
+
+
+
+
 
 
                 </p>
